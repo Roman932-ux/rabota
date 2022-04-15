4f88f9c882a48e2a05ca727a8053ac58d8bf361b
Your Name <you@example.com>
2022-04-15 12:03:02 +0300

    sortirovka pysyrek dobavlenie

--- rabota4.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-============== RESTART: C:/Users/Dell/Desktop/pythons.py =============
-[61, 49, 30, 63, 76, 11, 18, 47, 55, 15]
-[11, 15, 18, 30, 47, 49, 55, 61, 63, 76]
